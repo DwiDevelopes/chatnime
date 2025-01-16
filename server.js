f5c4d943dcaf3cf817a254c5e5bc690c7a3ccaad
Wolfes <166002502+DwiDevelopes@users.noreply.github.com>
2025-01-16 11:31:24 +0700

    main

--- server.js
+++ server.js
@@ -32,6 +32,13 @@ io.on('connection', (socket) => {
 });
 
 // Menjalankan server pada port 3000
-server.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+server.listen(3000, '0.0.0.0', () => {
+    console.log('Server running on http://0.0.0.0:3000');
 });
+
+
+// Menambahkan cors
+const cors = require('cors');
+app.use(cors({
+  origin: 'https://your-ngrok-subdomain.ngrok.io'  // Add Ngrok URL here
+}));
---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('chat-app-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          'index.html',
+          'LocalHost 3000.html',
+          'app.js',
+          'ico.png',
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
+      })
+    );
+  });
+  
